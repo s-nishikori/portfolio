4ec001bbeafff2b1e02c8f7e916bedcf9c1ae911
s-nishikori <nishikori6591@gmail.com>
2024-10-27 15:55:25 +0900

    add map key

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -8,8 +8,9 @@ const Projects: FC = () => {
     <div>
         <h1 className="h1 text-center p-5">My Projects</h1>
         <section className="container flex pt-10 pb-10 grid grid-cols-2 gap-10 xl:grid-cols-3">
-            {cardData.map((data) => (
+            {cardData.map((data, index) => (
                 <CardDemo
+                    key={index}
                     cardTitle={data.title}
                     cardDescription={data.description}
                     cardContent={data.content}
